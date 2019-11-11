9bd2aa691ab083884636d207b21d590d013572c0
mauslenter <marcela.auslenter@theappbusiness.com>
2019-11-11 17:22:37 +0000

    Initial Commit

---
+++ super.init.swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  super.init.swiftUI
+//
+//  Created by Marcela Auslenter on 11/11/2019.
+//  Copyright © 2019 Marcela Auslenter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
